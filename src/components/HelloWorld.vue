0d10e8841fdf94384b0a004ca308e0ab16669870
Yusuf Andriyanto <yusufxx33@gmail.com>
2023-07-07 16:54:11 +0700

    Update - wording style for mobile

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,15 +23,15 @@ export default defineComponent({
 <template>
   <div class="flex items-center h-screen p-5">
     <div class="px-10">
-      <p class="text-5xl">Hello, I'm Yusuf a <span class="border-b">Backend Developer</span></p>
+      <p class="text-xl lg:text-5xl">Hello, I'm Yusuf a <span class="border-b text-5xl leading-tight">Backend Developer</span></p>
       <br>>><button type="button" class="ml-4 py-2 border-b border-transparent hover:border-white transition-all">See my works</button>
       <br>>><button type="button" class="ml-4 py-2 border-b border-transparent hover:border-white transition-all text-blue-200">Connect with me on linkedin</button>
       <br> <span class="text-xs ml-9 text-gray-500"> Or follow my social media </span>
       <!-- <br>>><button type="button" class="px-4 py-3 hover:border-b transition-all">See my works</button> -->
       <div class="text-xs ml-9 mt-2">
-        <button type="button" class="px-4 mr-2 py-3 border hover:bg-white transition-all hover:text-black">Instagram</button>
-        <button type="button" class="px-4 mr-2 py-3 border hover:bg-white transition-all hover:text-black">Twitter</button>
-        <button type="button" class="px-4 mr-2 py-3 border hover:bg-white transition-all hover:text-black">Threads</button>
+        <button type="button" class="px-4 mr-2 py-3 border hover:bg-white transition-all hover:text-black mt-2">Instagram</button>
+        <button type="button" class="px-4 mr-2 py-3 border hover:bg-white transition-all hover:text-black mt-2">Twitter</button>
+        <button type="button" class="px-4 mr-2 py-3 border hover:bg-white transition-all hover:text-black mt-2">Threads</button>
       </div>
     </div>
   </div>
